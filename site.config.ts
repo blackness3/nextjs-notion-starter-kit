ad655f0a8468a6252b98bf33cc51b25d6fed8c9d
blackness3 <57695239+blackness3@users.noreply.github.com>
2024-05-05 12:39:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: '90c6403089d54d2493b310a5945dd1d2'
+     },
+     {
+       title: 'Contact',
+       pageId: '7b911e9d5088407183693285134f4997'
+     }
+   ]
 })
