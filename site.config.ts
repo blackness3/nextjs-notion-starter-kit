4ca927e2e3199c519f537496c7d5d6c91ab59096
blackness3 <57695239+blackness3@users.noreply.github.com>
2024-05-05 12:23:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -59,7 +59,7 @@ export default siteConfig({
      },
      {
        title: 'Contact',
-       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+       pageId: '7b911e9d5088407183693285134f4997'
      }
    ]
 })
